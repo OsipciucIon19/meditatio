5d56b730055cfc485c5e26f48fa1277a962eaf62
OsipciucIon19 <ion-osipciuc@mail.ru>
2023-04-05 09:01:01 +0300

    implement schedule functionality

--- client/src/components/modal/ScheduleModal.tsx
+++ client/src/components/modal/ScheduleModal.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Button, Form, Input, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 import Localization from "react-widgets/Localization";
 import {DateLocalizer} from "react-widgets/IntlLocalizer";
-import DatePicker from "react-widgets/DatePicker";
+import DatePicker from "react-widgets/DatePicker"
 import {Course} from "../../types/course";
 
 type ScheduleModalProps = {
@@ -13,7 +13,91 @@ type ScheduleModalProps = {
 }
 const ScheduleModal: FC<ScheduleModalProps> = (props) => {
     const {modal, toggle, event, course} = props
+    console.log(event)
     const [, setSelectedEvent] = useState(event)
+    const [toEventHours, setToEventHours] = useState<number>(event.getHours())
+    const [fromEventHours, setFromEventHours] = useState<number>(event.getHours() + 1)
+    const [toEventMinutes, setToEventMinutes] = useState<number>(event.getMinutes())
+    const [fromEventMinutes, setFromEventMinutes] = useState<number>(event.getMinutes())
+    const [daysAmountSelected, setDaysAmountSelected] = useState('single')
+    const monthNames = ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie",
+        "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"
+    ]
+
+    const singleSelectedElements = (
+        <div>
+            <p><b>{`${event.getDate()} ${monthNames[event.getMonth()]}`}</b></p>
+            <section className="d-flex">
+                <div className="d-flex align-items-center">
+                    <label className="w-25 me-2">
+                        De la:
+                    </label>
+                    <div className="d-flex align-items-center">
+                        <Input
+                            className="w-50"
+                            type="number"
+                            min="0"
+                            max="23"
+                            defaultValue={event.getHours()}
+                            value={toEventHours < 10 ? `0${toEventHours}` : toEventHours}
+                            onChange={(event) => setToEventHours(parseInt(event.target.value))}
+                        />
+                        <span className="me-1">:</span>
+                        <Input
+                            className="w-50"
+                            type="number"
+                            min="0"
+                            max="59"
+                            value={toEventMinutes < 10 ? `0${toEventMinutes}` : toEventMinutes}
+                            onChange={(event) => setToEventMinutes(parseInt(event.target.value))}
+                        />
+                    </div>
+                </div>
+                <div className="d-flex align-items-center me-2">
+                    <label className="w-50">
+                        Până la:
+                    </label>
+                    <div className="d-flex align-items-center">
+                        <Input
+                            className="w-50"
+                            type="number"
+                            min="0"
+                            max="23"
+                            value={fromEventHours < 10 ? `0${fromEventHours}` : fromEventHours}
+                            onChange={(event) => setFromEventHours(parseInt(event.target.value))}
+                        />
+                        <span className="me-1">:</span>
+                        <Input
+                            className="w-50"
+                            type="number"
+                            min="0"
+                            max="59"
+                            value={fromEventMinutes < 10 ? `0${fromEventMinutes}` : fromEventMinutes}
+                            onChange={(event) => setFromEventMinutes(parseInt(event.target.value))}
+                        />
+                    </div>
+                </div>
+            </section>
+            <DatePicker
+                value={event}
+                onChange={(value) => setSelectedEvent(value)}
+                includeTime
+            />
+            <label>
+                Până la:
+            </label>
+            <DatePicker
+                value={new Date(event.getTime() + 60 * 60 * 1000)}
+                disabled
+                includeTime
+            />
+        </div>
+    )
+    const multipleSelectedElements = <div>multiple</div>
+
+    const handleSelectChange = (event) => {
+        setDaysAmountSelected(event.target.value)
+    }
 
     return (
         <Modal isOpen={modal} toggle={toggle}>
@@ -22,37 +106,37 @@ const ScheduleModal: FC<ScheduleModalProps> = (props) => {
                 <Localization date={new DateLocalizer({culture: 'ro', firstOfWeek: 1})}>
                     <Form>
                         <div>Title: {course?.subject.title} clasa {course?.subject.grade}</div>
-                        <hr/>
-                        <div>student id: {}</div>
-                        <div>teacher id: {}</div>
                         <div>course id: {course?._id}</div>
                         <hr/>
-                        <label>
-                            From:
-                        </label>
-                        <DatePicker
-                            value={event}
-                            onChange={(value) => setSelectedEvent(value)}
-                            includeTime
-                        />
-                        <label>
-                            To:
-                        </label>
-                        <DatePicker
-                            value={new Date(event.getTime() + 60 * 60 * 1000)}
-                            disabled
-                            includeTime
-                        />
+                        <div className="d-flex align-items-center mb-3">
+                            <label className="me-4" htmlFor="daysAmount">
+                                Programează:
+                            </label>
+                            <Input
+                                type="select"
+                                name="daysAmount"
+                                id="daysAmount"
+                                value={daysAmountSelected}
+                                onChange={handleSelectChange}
+                            >
+                                <option value="single">o singură lecție</option>
+                                <option value="multiple">mai multe lecții</option>
+                            </Input>
+                        </div>
+                        {daysAmountSelected === 'single' && singleSelectedElements}
+                        {daysAmountSelected === 'multiple' && multipleSelectedElements}
                     </Form>
                 </Localization>
             </ModalBody>
             <ModalFooter>
-                {/*<Button color="primary" onClick={() => setIsModalOpen(false)}>*/}
-                {/*    Programeaza*/}
-                {/*</Button>*/}
-                {/*<Button color="danger" onClick={() => setIsModalOpen(false)}>*/}
-                {/*    Cancel*/}
-                {/*</Button>*/}
+                <Button color="primary" onClick={() => {
+                }}>
+                    Programeaza
+                </Button>
+                <Button color="danger" onClick={() => {
+                }}>
+                    Cancel
+                </Button>
             </ModalFooter>
         </Modal>
     )
